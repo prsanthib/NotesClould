ab8f8b3dfc44bfcc3566945644411f144a008a5b
prsanthib <prasanthibammidi10@gmail.com>
2024-06-28 02:20:52 +0530

    fina;

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
+const path = require('path');
+
 const cors = require('cors');
 
 
@@ -15,6 +17,14 @@ app.use(cors())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+// Serve static files from the React app
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Serve the React app for any other route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`iNotebook app listening at http://localhost:${port}`)
